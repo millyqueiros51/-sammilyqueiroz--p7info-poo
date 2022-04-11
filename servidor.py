c171d046fad06e5c78a3fa5d043852f718b99061
Sammily Queirós Osório <99552976+millyqueiros51@users.noreply.github.com>
2022-04-11 10:47:54 -0300

    Create servidor.py

---
+++ servidor.py
@@ -0,0 +1,28 @@
+import json
+import socket
+
+from funcoes import *
+from unidades import *
+
+HOST, PORT = 'localhost', 5000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+
+while True:
+    s.listen()
+
+    clientsocket, endereco = s.accept()
+
+    data = clientsocket.recv(1024)
+    data = json.loads(data)
+
+    tipo = list(unidades.keys())[data[0] - 1]
+
+    if (tipo == 'distancia'):
+        valor = distancia(data[1], data[2], data[3])
+    elif (tipo == 'tempo'):
+        valor = tempo(data[1], data[2], data[3])
+
+    clientsocket.send(json.dumps(valor).encode('utf-8'))
+    clientsocket.close()
